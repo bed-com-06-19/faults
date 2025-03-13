6be0305a98b7ca08ba136ce1fd430610777fac17
ZAKEYU MATIAS <BSC/MAT/01/19@COMLAB.COM>
2025-03-13 22:09:43 +0200

    Set up Firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart'; // For kIsWeb
 import 'pages/landing_page.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase only once depending on the platform
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: "AIzaSyBfyyLutVWu8cOHtZS8NtnJf3IeH3Rx1xI",
+        appId: "1:664103674812:web:4408c024503b178b2e01b9",
+        messagingSenderId: "664103674812",
+        projectId: "faults-2a6ef",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const FaultDetectionApp());
 }
 
--- lib/pages/code_entry_page.dart
+++ lib/pages/code_entry_page.dart
@@ -5,6 +5,7 @@ class CodeEntryPage extends StatefulWidget {
   const CodeEntryPage({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _CodeEntryPageState createState() => _CodeEntryPageState();
 }
 
